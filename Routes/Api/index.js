bc507390fdfacbbead880b037eaec4baddc27d78
Arnab Chakraborty <71896815+codebook-2000@users.noreply.github.com>
2021-12-18 21:48:28 +0530

    Updated index.js

--- Routes/Api/index.js
+++ Routes/Api/index.js
@@ -1,7 +1,5 @@
 const route = require('express').Router();
 
-// These would be triggered from the HTML page which is rendered as 
-// a interface at the very start
 // route.use('/update', require('./update').route);
 
 route.use('/display', require('./display').route);
